deb45117132e5c1479d77e83718e761a146098cf
Arnaldo Pedroso <pedroso.arnaldo@gmail.com>
2017-01-02 18:48:25 -0200

    site until transport

---
+++ site_ate_tr.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+
+### Altered database configuration
+use DBI;
+use DBD::mysql;
+use IO::Socket::INET;
+use POSIX qw(ceil floor);
+
+### Database configuration
+my $dsn  = 'DBI:mysql:usina:10.17.0.5:3306';
+my $user = 'usina_usr';
+my $pass = 'zse4nji9';
+###
+
+### Open port for zabbix monitoring
+$socket = new IO::Socket::INET (
+  LocalHost => '10.17.0.5',
+  LocalPort => '5003',
+  Proto => 'tcp',
+  Listen => 5,
+  Reuse => 1
+) or die "ERROR in Socket Creation : $!\n";
+
+### Initialize database
+$dbh = DBI->connect($dsn, $user, $pass) or die "DBI::errstr\n\n";
+
+### insert one line each minute
+while() {
+  
+  ## getting localtime
+  my $time = do {
+    my ($s,$m,$h,$D,$M,$Y)=localtime;
+    $Y+=1900;
+    $M++;
+    "$Y-$M-$D $h:$m" };
+
+  my ($s,$m,$h,$D,$M,$Y)=localtime;
+
+  $query = $dbh->prepare('select quantidade_minutos + 1 from NF_Emitida where data_integracao = ( select max(data_integracao) from Pedidos_Aprovados )');
+  $query->execute();
+
+  while (@results = $query->fetchrow_array()) {
+    $randon_number = $results[0];
+  }
+
+  $query = "INSERT INTO Site_Transportadora (data_integracao, quantidade_minutos)  VALUES(?, ?)";
+  $statement = $dbh->prepare($query);
+  $statement->execute($time,$randon_number);
+
+  ### Generate log
+#  open $log, ">>", "/var/log/gera_pedidos_integrados/stdout.log" or die "Can't open the fscking file: $!";  
+
+#  print $log "server1.intranet,$time,$randon_number\n";
+
+  $system_variable = "zabbix_sender -z stark -s server6.intranet -k TR -o $randon_number";
+
+  system($system_variable);
+#  close $log;
+
+  sleep(3600); 
+}
+
